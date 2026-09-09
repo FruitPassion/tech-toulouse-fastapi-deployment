18348b43b7e52f4909fbd138abd9101ac7e88692
FruitPassion <gamehotdl@gmail.com>
2026-09-09 23:15:44 +0200

    fix: Modify database url to fit deployement requirements

--- app/database.py
+++ app/database.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./users.db"
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(os.environ["DATABASE_URL"])
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
